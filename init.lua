8eabb7e77565149e9f57a27052f2315e5bb18af0
Gregor Parzefall <gregor.parzefall@posteo.de>
2022-11-13 11:40:16 +0100

    Lots of repetition

--- init.lua
+++ init.lua
@@ -43,6 +43,74 @@ local DYE_COLORS = {
 
 local player_lasts = {}
 
+local function get_canvas(pos, box, face_normal)
+    local box_center = box:get_center()
+
+    local canvas_rot = vector.dir_to_rotation(face_normal)
+    local rot_box = aabb.new(
+        box.pos_min:rotate(canvas_rot),
+        box.pos_max:rotate(canvas_rot)
+    )
+    rot_box:repair()
+    local rot_box_size = rot_box:get_size()
+
+    local canvas_pos = pos +
+        box_center +
+        vector.new(0, 0, rot_box_size.z * 0.5 + 0.001):rotate(canvas_rot)
+
+    local objs = minetest.get_objects_inside_radius(canvas_pos, 0.001)
+    for _, obj in ipairs(objs) do
+        local ent = obj:get_luaentity()
+        if ent and ent.name == "ggraffiti:canvas" then
+            return ent
+        end
+    end
+end
+
+local function create_canvas(pos, box, face_normal)
+    local box_center = box:get_center()
+
+    local canvas_rot = vector.dir_to_rotation(face_normal)
+    local rot_box = aabb.new(
+        box.pos_min:rotate(canvas_rot),
+        box.pos_max:rotate(canvas_rot)
+    )
+    rot_box:repair()
+    local rot_box_size = rot_box:get_size()
+
+    local canvas_pos = pos +
+        box_center +
+        vector.new(0, 0, rot_box_size.z * 0.5 + 0.001):rotate(canvas_rot)
+
+    local obj = minetest.add_entity(canvas_pos, "ggraffiti:canvas")
+    obj:set_rotation(canvas_rot)
+    local ent = obj:get_luaentity()
+    ent:setup({x = rot_box_size.x, y = rot_box_size.y})
+    return ent
+end
+
+local function get_point_on_canvas(pos, box, face_normal, point)
+    local box_center = box:get_center()
+
+    local canvas_rot = vector.dir_to_rotation(face_normal)
+    local rot_box = aabb.new(
+        box.pos_min:rotate(canvas_rot),
+        box.pos_max:rotate(canvas_rot)
+    )
+    rot_box:repair()
+    local rot_box_size = rot_box:get_size()
+
+    local root_pos = pos +
+        box_center +
+        vector.new(0, 0, rot_box_size.z * 0.5):rotate(canvas_rot)
+
+    local distance = point - root_pos
+
+    -- 2D (Z is always zero)
+    return vector.new(-distance.x, -distance.y, distance.z):rotate(canvas_rot) +
+        vector.new(rot_box_size.x / 2, rot_box_size.y / 2, 0)
+end
+
 local function spraycast(player, pos, dir, def)
     local ray = minetest.raycast(pos, pos + dir * MAX_SPRAY_DISTANCE, true, false)
     local pthing
@@ -54,9 +122,9 @@ local function spraycast(player, pos, dir, def)
     end
     if not pthing or pthing.type ~= "node" then return end
 
-    local node_pos = pthing.under
-    if minetest.is_protected(node_pos, player:get_player_name()) then
-        minetest.record_protection_violation(node_pos, player:get_player_name())
+    local pos = pthing.under
+    if minetest.is_protected(pos, player:get_player_name()) then
+        minetest.record_protection_violation(pos, player:get_player_name())
         return
     end
 
@@ -65,51 +133,21 @@ local function spraycast(player, pos, dir, def)
     local raw_box = modlib.minetest.get_node_selectionboxes(pthing.under)[pthing.box_id]
     if not raw_box then return end -- Modlib failed 😱
     local box = aabb.from(raw_box)
-    box:repair()
-    local box_center = box:get_center()
 
-    local canvas_rot = vector.dir_to_rotation(pthing.intersection_normal)
-    local rot_box = aabb.new(
-        box.pos_min:rotate(canvas_rot),
-        box.pos_max:rotate(canvas_rot)
-    )
-    rot_box:repair()
-    local rot_box_size = rot_box:get_size()
-
-    local canvas_pos = node_pos + box_center + vector.new(0, 0, rot_box_size.z * 0.5 + 0.001):rotate(canvas_rot)
-    local canvas
-
-    local findings = minetest.get_objects_inside_radius(canvas_pos, 0.001)
-    for _, fobj in ipairs(findings) do
-        local fent = fobj:get_luaentity()
-        if fent and fent.name == "ggraffiti:canvas" then
-            canvas = fent
-            break
-        end
-    end
-
+    local canvas = get_canvas(pos, box, pthing.intersection_normal)
     if not canvas then
         if def.anti then return end
-
-        local obj = minetest.add_entity(canvas_pos, "ggraffiti:canvas")
-        obj:set_rotation(canvas_rot)
-        canvas = obj:get_luaentity()
-        canvas:setup({x = rot_box_size.x, y = rot_box_size.y})
+        canvas = create_canvas(pos, box, pthing.intersection_normal)
     end
 
-    local root_pos = node_pos + box_center + vector.new(0, 0, rot_box_size.z * 0.5):rotate(canvas_rot)
-    local pointed_pos = pthing.intersection_point
-    local distance = pointed_pos - root_pos
-
-    local pos_on_face = vector.new(-distance.x, -distance.y, distance.z):rotate(canvas_rot) -- 2D (Z is always zero)
-    pos_on_face = pos_on_face + vector.new(rot_box_size.x / 2, rot_box_size.y / 2, 0)
-
-    local pos_on_bitmap = vector.new( -- 2D too, of course
-        math.floor(pos_on_face.x / rot_box_size.x * canvas.bitmap_size.x),
-        math.floor(pos_on_face.y / rot_box_size.y * canvas.bitmap_size.y),
+    local point = get_point_on_canvas(
+        pos, box, pthing.intersection_normal, pthing.intersection_point)
+    local pixel = vector.new(
+        math.floor(point.x / canvas.size.x * canvas.bitmap_size.x),
+        math.floor(point.y / canvas.size.y * canvas.bitmap_size.y),
         0
     )
-    local index = pos_on_bitmap.y * canvas.bitmap_size.x + pos_on_bitmap.x + 1
+    local index = pixel.y * canvas.bitmap_size.x + pixel.x + 1
 
     if def.anti then
         if canvas.bitmap[index] ~= TRANSPARENT then
