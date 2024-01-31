8166241503f4b880be2e53ce7ae6ce87f3387405
Gregor Parzefall <gregor.parzefall@posteo.de>
2024-01-31 19:48:20 +0100

    GUI stuff
    Flow has built-in server-side translation support now.

--- src/gui.lua
+++ src/gui.lua
@@ -8,17 +8,6 @@ local FORMSPEC_PADDING = 0.4
 local FORMSPEC_SPACING = 0.25
 local FORMSPEC_LOWSPACING = 0.1
 
--- In formspecs, I use server-side translations so that the layout of the
--- formspecs can adapt to the length of the translated strings.
-local function ServerSDouble(player, string)
-    local player_name = player:get_player_name()
-    local lang_code = minetest.get_player_information(player_name).lang_code
-    return minetest.get_translated_string(lang_code, string)
-end
-local function ServerS(player, string, ...)
-    return ServerSDouble(player, S(string, ...))
-end
-
 function shared.get_raw_spray_def(item)
     local def = item:get_definition()
     return def and def._ggraffiti_spray_can
@@ -45,10 +34,10 @@ local function update_item_meta(item, meta)
         meta:set_string("description",
             item:get_short_description() .. "\n" ..
             S("Left-click to spray, right-click to configure.") .. "\n\n" ..
-            S("Color: ") .. color_block .. S(" @1, @2, @3", color.r, color.g, color.b) .. "\n" ..
+            S("Color: @1 @2, @3, @4", color_block, color.r, color.g, color.b) .. "\n" ..
             S("Size: @1", size)
         )
-        -- 5.8.0-dev feature
+        -- 5.8.0 feature
         meta:set_string("inventory_image", shared.get_colored_can_texmod(color_str))
     else
         meta:set_string("description",
@@ -79,8 +68,7 @@ local function get_color(item_name)
 end
 
 local function make_color_texture(color)
-    local png = minetest.encode_png(1, 1, { color }, 9)
-    return "[png:" .. minetest.encode_base64(png)
+    return "[fill:1x1:" .. minetest.colorspec_to_colorstring(color)
 end
 
 local function is_correct_item(ctx, item)
@@ -159,17 +147,16 @@ gui_configure = flow.make_gui(function(player, ctx)
         padding = FORMSPEC_PADDING,
         spacing = FORMSPEC_PADDING,
 
-        gui.label { label = ServerSDouble(player, ctx.item_desc) },
+        gui.label { label = ctx.item_desc },
         color and gui.HBox {
             spacing = FORMSPEC_SPACING,
             gui.VBox {
                 spacing = 0,
                 expand = true,
                 align_h = "left",
-                gui.Label { label = ServerS(player, "Color") },
+                gui.Label { label = S("Color") },
                 gui.Label {
-                    label = ServerS(player, "R: @1, G: @2, B: @3",
-                        color.r, color.g, color.b),
+                    label = S("R: @1, G: @2, B: @3", color.r, color.g, color.b),
                 },
             },
             gui.Image {
@@ -178,7 +165,7 @@ gui_configure = flow.make_gui(function(player, ctx)
                 texture_name = make_color_texture(color),
             },
             ctx.is_rgb and gui.Button {
-                label = ServerS(player, "Change"),
+                label = S("Change"),
                 on_event = function(player, ctx)
                     gui_change_rgb_color:show(player, {
                         item_name = ctx.item_name,
@@ -193,7 +180,7 @@ gui_configure = flow.make_gui(function(player, ctx)
         gui.HBox {
             spacing = FORMSPEC_SPACING,
             gui.Label {
-                label = ServerS(player, "Size"),
+                label = S("Size"),
                 expand = true,
                 align_h = "left",
             },
@@ -256,9 +243,9 @@ end
 gui_change_rgb_color = flow.make_gui(function(player, ctx)
     local has_input_color = not not (ctx.form.field_r and ctx.form.field_g and ctx.form.field_b)
     local has_default_color = not ctx.initial_setup
-    local png_color
+    local preview_color
     if has_input_color or has_default_color then
-        png_color = {
+        preview_color = {
             r = has_input_color and tonumber(ctx.form.field_r) or ctx.rgb_color.r,
             g = has_input_color and tonumber(ctx.form.field_g) or ctx.rgb_color.g,
             b = has_input_color and tonumber(ctx.form.field_b) or ctx.rgb_color.b,
@@ -271,9 +258,7 @@ gui_change_rgb_color = flow.make_gui(function(player, ctx)
         spacing = FORMSPEC_PADDING,
 
         gui.Label {
-            label = ctx.initial_setup and
-                ServerS(player, "Set Color") or
-                ServerS(player, "Change Color"),
+            label = ctx.initial_setup and S("Set Color") or S("Change Color"),
         },
         gui.VBox {
             spacing = FORMSPEC_LOWSPACING,
@@ -281,7 +266,7 @@ gui_change_rgb_color = flow.make_gui(function(player, ctx)
                 spacing = FORMSPEC_SPACING,
                 gui.Field {
                     name = "field_r",
-                    label = minetest.colorize("#f00", ServerS(player, "R (Red)")),
+                    label = minetest.colorize("#f00", S("R (Red)")),
                     default = not ctx.initial_setup and tostring(ctx.rgb_color.r) or nil,
                     expand = true,
                     on_event = function(player, ctx)
@@ -291,7 +276,7 @@ gui_change_rgb_color = flow.make_gui(function(player, ctx)
                 },
                 gui.Field {
                     name = "field_g",
-                    label = minetest.colorize("#0f0", ServerS(player, "G (Green)")),
+                    label = minetest.colorize("#0f0", S("G (Green)")),
                     default = not ctx.initial_setup and tostring(ctx.rgb_color.g) or nil,
                     expand = true,
                     on_event = function(player, ctx)
@@ -301,7 +286,7 @@ gui_change_rgb_color = flow.make_gui(function(player, ctx)
                 },
                 gui.Field {
                     name = "field_b",
-                    label = minetest.colorize("#00f", ServerS(player, "B (Blue)")),
+                    label = minetest.colorize("#00f", S("B (Blue)")),
                     default = not ctx.initial_setup and tostring(ctx.rgb_color.b) or nil,
                     expand = true,
                     on_event = function(player, ctx)
@@ -310,24 +295,24 @@ gui_change_rgb_color = flow.make_gui(function(player, ctx)
                     end,
                 },
             },
-            gui.Label { label = ServerS(player, "Values must be integers between 0 and 255.") },
+            gui.Label { label = S("Values must be integers between 0 and 255.") },
         },
         gui.VBox {
             spacing = 0,
-            gui.Label { label = ServerS(player, "Preview") },
+            gui.Label { label = S("Preview") },
             gui.HBox {
                 spacing = FORMSPEC_SPACING,
-                png_color and gui.Image {
+                preview_color and gui.Image {
                     w = 0.8,
                     h = 0.8,
                     expand = true,
                     align_h = "fill",
-                    texture_name = make_color_texture(png_color),
+                    texture_name = make_color_texture(preview_color),
                 } or gui.Nil {},
                 gui.Button {
-                    label = ServerS(player, "Update"),
-                    expand = not png_color or nil,
-                    align_h = not png_color and "right" or nil,
+                    label = S("Update"),
+                    expand = not preview_color or nil,
+                    align_h = not preview_color and "right" or nil,
                     -- no on_event needed
                 },
             },
@@ -335,12 +320,12 @@ gui_change_rgb_color = flow.make_gui(function(player, ctx)
         gui.HBox {
             spacing = FORMSPEC_SPACING,
             ctx.initial_setup and gui.Nil {} or gui.Button {
-                label = ServerS(player, "Cancel"),
+                label = S("Cancel"),
                 expand = true,
                 on_event = cancel_button_on_event,
             },
             gui.Button {
-                label = ServerS(player, "Save"),
+                label = S("Save"),
                 expand = true,
                 on_event = save_button_on_event,
             },
